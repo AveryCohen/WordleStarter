2cee44b061ade5c38e8020e24b0078065bfcc62d
AveryCohen <AveryCohen04@gmail.com>
2022-05-10 14:28:53 -0400

    finished part 1

--- Wordle.java
+++ Wordle.java
@@ -7,8 +7,14 @@
 public class Wordle {
 
     public void run() {
+        String currentLetter = "";
         gw = new WordleGWindow();
         gw.addEnterListener((s) -> enterAction(s));
+        String word = WordleDictionary.FIVE_LETTER_WORDS[(int)(Math.random() * (WordleDictionary.FIVE_LETTER_WORDS.length))];
+        for (int i = 0; i < WordleGWindow.N_COLS; i++) {
+                currentLetter = word.substring(i, i + 1);
+            gw.setSquareLetter(0, i, currentLetter);
+        }
     }
 
 /*
